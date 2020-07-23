a1ed036abf5213e2480fd0f5ff68b2562f3d3447
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-23 22:38:51 +0200

    updated notes

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('App executed.');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello React Native</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
---
+++ notes.js
@@ -0,0 +1,6 @@
+/* 
+When adding SDK to bash-profile and the platform tools, be sure to open a new terminal before y
+the adb command. 
+
+
+*/
