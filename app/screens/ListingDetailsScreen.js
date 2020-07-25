dc0a2e658af7a70dfe6db32a6a62dd54e4718a43
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 18:15:26 +0200

    applied minor styling to Listings

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -12,11 +12,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
-        <ListItem
-          image={require('../assets/JasonPF.jpg')}
-          title='Jason Nelemans'
-          subTitle='5 Listings'
-        />
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/JasonPF.jpg')}
+            title='Jason Nelemans'
+            subTitle='5 Listings'
+          />
+        </View>
       </View>
     </View>
   );
@@ -39,6 +41,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '500',
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
