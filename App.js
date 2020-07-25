3644c7e9628fd88d3f5e333efcd18420b0f9410d
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-26 00:26:43 +0200

    created PickerItem component

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import Screen from './app/components/Screen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+  { label: 'furniture', value: 1 },
+  { label: 'jasdasd', value: 2 },
+  { label: 'furgggre', value: 3 },
+];
+
 export default function App() {
   return (
     <Screen>
-      <AppPicker icon='apps' placeholder='Category' />
+      <AppPicker items={categories} icon='apps' placeholder='Category' />
       <AppTextInput icon='email' placeholder='Email' />
     </Screen>
   );
