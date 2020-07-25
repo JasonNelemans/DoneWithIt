3213a31a95669de5775ae4644b0b275715c4ee0b
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 18:11:34 +0200

    created simple ListItem component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 
 import colors from '../config/colors';
 
@@ -11,6 +12,11 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <ListItem
+          image={require('../assets/JasonPF.jpg')}
+          title='Jason Nelemans'
+          subTitle='5 Listings'
+        />
       </View>
     </View>
   );
