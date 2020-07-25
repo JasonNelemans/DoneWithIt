6e22bc31a0fa315939a39772e8ec6bf596c764c4
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 11:40:54 +0200

    testing debug session

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
   console.log('App executed.');
 
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text>Done with it</Text>
       <StatusBar style='auto' />
     </View>
   );
