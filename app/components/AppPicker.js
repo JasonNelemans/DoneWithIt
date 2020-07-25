9317e334c46bd6d71ea6dca614bd790d8f57836b
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 23:57:01 +0200

    created App picker component

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -3,8 +3,9 @@ import { View, TextInput, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles';
+import AppText from './AppText';
 
-function AppTextInput({ icon, ...otherProps }) {
+function AppPicker({ icon, placeholder, ...otherProps }) {
   return (
     <View style={styles.container}>
       {icon && (
@@ -15,7 +16,12 @@ function AppTextInput({ icon, ...otherProps }) {
           style={styles.icon}
         />
       )}
-      <TextInput style={defaultStyles.text} {...otherProps} />
+      <AppText style={styles.text}>{placeholder}</AppText>
+      <MaterialCommunityIcons
+        name='chevron-down'
+        size={20}
+        color={defaultStyles.colors.medium}
+      />
     </View>
   );
 }
@@ -32,6 +38,9 @@ const styles = StyleSheet.create({
   icon: {
     marginRight: 10,
   },
+  text: {
+    flex: 1,
+  },
   textInput: {
     color: defaultStyles.colors.dark,
     fontSize: 18,
@@ -39,4 +48,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AppTextInput;
+export default AppPicker;
