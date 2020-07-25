98a48245f8d75a0418ec8236388e0a4aa62e3aa0
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 15:14:30 +0200

    created colors file

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
+
+// Screens
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
