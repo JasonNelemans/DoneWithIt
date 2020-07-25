7fd123b03e81187c35877f93dc60b82e95b6449c
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-25 15:52:38 +0200

    rendered simple Login ButtonComponent

--- App.js
+++ App.js
@@ -4,7 +4,17 @@ import { View } from 'react-native';
 // Screens
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <AppButton title='Login' />
+    </View>
+  );
 }
