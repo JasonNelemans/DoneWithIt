adc9cfded8c6fafeeca31fd5619dacf49ea966eb
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-26 16:36:06 +0200

    renamed files

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -5,7 +5,7 @@ import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import colors from '../../config/colors';
 
-import AppText from '../AppText';
+import Text from '../Text';
 
 function ListItem({
   title,
@@ -22,13 +22,13 @@ function ListItem({
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title} numberOfLines={1}>
+            <Text style={styles.title} numberOfLines={1}>
               {title}
-            </AppText>
+            </Text>
             {subTitle && (
-              <AppText style={styles.subTitle} numberOfLines={2}>
+              <Text style={styles.subTitle} numberOfLines={2}>
                 {subTitle}
-              </AppText>
+              </Text>
             )}
           </View>
           <MaterialCommunityIcons
