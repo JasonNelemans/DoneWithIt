6e42a7a6c2357cd5d4ee1309d929a6b9b43edae8
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-26 13:58:24 +0200

    created ListingEditScreen

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-import Screen from './app/components/Screen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-  return <RegisterScreen />;
+  return <ListingEditScreen />;
 }
