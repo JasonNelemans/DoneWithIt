f8b35d3a4e584b2fe7a3beba6423f6285043fd92
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-26 16:19:31 +0200

    styled and customized category items

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -18,6 +18,7 @@ function AppPicker({
   icon,
   items,
   onSelectItem,
+  numberOfColumns = 1,
   PickerItemComponent = PickerItem,
   placeholder,
   selectedItem,
@@ -55,8 +56,10 @@ function AppPicker({
           <FlatList
             data={items}
             keyExtractor={(item) => item.value.toString()}
+            numColumns={numberOfColumns}
             renderItem={({ item }) => (
               <PickerItemComponent
+                item={item}
                 label={item.label}
                 onPress={() => {
                   setModalVisible(false);
