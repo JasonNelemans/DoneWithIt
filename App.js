256f40feb26ceecf535478d86577b16a21a9bf54
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-27 17:36:40 +0200

    testing ImagePicker

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import React, { useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return <WelcomeScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
