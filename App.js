20d0a4a2ce0f101a4af76224144123322ea525b1
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-28 15:19:10 +0200

    select image from library

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { Image, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
 
 import Screen from './app/components/Screen';
 
 export default function App() {
-  const requestPermission = async () => {
-    const { granted } = await Permissions.askAsync(
-      Permissions.CAMERA_ROLL,
-      Permissions.LOCATION
-    );
-    // const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert('You need to enable permission to access the library');
+  const [imageUri, setImageUri] = useState();
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log('error reading an image', error);
+    }
   };
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  return <Screen></Screen>;
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
