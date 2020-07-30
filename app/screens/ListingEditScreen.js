5952b2cbf49b9711bebf74758521b72009fe5ed7
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-30 20:35:22 +0200

    updated to include location

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
+import * as Location from 'expo-location';
 
 import {
   AppForm as Form,
@@ -46,6 +47,20 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+  const [location, setLocation] = useState();
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestPermissionsAsync();
+    if (!granted) return;
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getLastKnownPositionAsync();
+    setLocation({ latitude, longitude });
+  };
+
+  useEffect(() => {
+    getLocation();
+  });
   return (
     <Screen style={styles.container}>
       <Form
@@ -56,7 +71,7 @@ function ListingEditScreen() {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}>
         <FormImagePicker name='images' />
         <FormField maxLength={255} name='title' placeholder='Title' />
