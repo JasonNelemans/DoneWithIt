5bdf47ef170edad5409b8920d9107e6a1760f5ff
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-01 13:41:42 +0200

    tested custom header styles

--- App.js
+++ App.js
@@ -31,13 +31,13 @@ const TweetDetails = ({ route }) => (
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: 'dodgerblue' },
+      headerTintColor: 'white',
+    }}>
     <Stack.Screen name='Tweets' component={Tweets} />
-    <Stack.Screen
-      name='TweetDetails'
-      component={TweetDetails}
-      options={({ route }) => ({ title: route.params.id })}
-    />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
   </Stack.Navigator>
 );
 
