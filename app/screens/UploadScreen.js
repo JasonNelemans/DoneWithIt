03d8f2a648d6fb9e67f6929f34b2373814aeb3ef
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-03 18:09:06 +0200

    conditionally render done animation

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { View, StyleSheet, Modal } from 'react-native';
 import * as Progress from 'react-native-progress';
-
-import Text from '../components/Text';
+import LottieView from 'lottie-react-native';
 
 function UploadScreen({ progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Progress.Bar color={colors.primary} progress={progress} width={200} />
+        {progress < 1 ? (
+          <Progress.Bar
+            color={colors.primary}
+            progress={progress}
+            width={200}
+          />
+        ) : (
+          <LottieView
+            autoPlay
+            loop={false}
+            source={require('../assets/animations/done.json')}
+            style={styles.animation}
+          />
+        )}
       </View>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  animation: {
+    width: 150,
+  },
   container: {
     alignItems: 'center',
     flex: 1,
