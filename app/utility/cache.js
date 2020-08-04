6b46c0dc031097565b83a6f6bd97d601b5d5bad3
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-04 12:05:07 +0200

    finished cache layer

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,6 +1,8 @@
 import AsyncStorage from '@react-native-community/async-storage';
+import moment from 'moment';
 
 const prefix = 'cache';
+const expiryInMinutes = 5;
 
 const store = async (key, value) => {
   try {
@@ -14,12 +16,25 @@ const store = async (key, value) => {
   }
 };
 
+const isExpired = (item) => {
+  const now = moment(Date.now());
+  const storedtime = moment(item.timestamp);
+  return now.diff(storedtime, 'minutes') > expiryInMinutes;
+};
+
 const get = async (key) => {
   try {
     const value = await AsyncStorage.getItem(prefix + key);
     const item = JSON.parse(value);
 
     if (!item) return null;
+
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+
+    return item.value;
   } catch (error) {
     console.log(error);
   }
