20d421aa7f961e479c8f4d28e051dd19cfe13b78
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-04 11:32:16 +0200

    expo install @react-native-community/async-storage

--- App.js
+++ App.js
@@ -1,69 +1,8 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-
-import Screen from './app/components/Screen';
-import AuthNavigator from './app/navigation/AuthNavigator';
-import NavigationTheme from './app/navigation/NavigationTheme';
-import AppNavigator from './app/navigation/AppNavigator';
-
-const Link = () => {
-  const navigation = useNavigation();
-
-  return (
-    <Button
-      title='Click'
-      onPress={() => navigation.navigate('TweetDetails', { id: 1 })}
-    />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: 'dodgerblue' },
-      headerTintColor: 'white',
-    }}>
-    <Stack.Screen name='Tweets' component={Tweets} />
-    <Stack.Screen name='TweetDetails' component={TweetDetails} />
-  </Stack.Navigator>
-);
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name='Feed' component={FeedNavigator} />
-    <Tab.Screen name='Account' component={Account} />
-  </Tab.Navigator>
-);
+import NetInfo from '@react-native-community/netinfo';
 
 export default function App() {
-  return (
-    <NavigationContainer theme={NavigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  NetInfo.addEventListener((netInfo) => console.log(netInfo));
+
+  return null;
 }
