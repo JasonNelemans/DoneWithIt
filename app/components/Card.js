9b2886f23916f4ae0177db6272715af01a6e56e7
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-04 13:03:27 +0200

    stored images in file system

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Image } from 'react-native-expo-image-cache';
 
 import colors from '../config/colors';
 import Text from './Text';
@@ -13,7 +9,7 @@ function Card({ title, subTitle, imageUrl, onPress }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image style={styles.image} uri={imageUrl} />
         <View style={styles.detailsContainer}>
           <Text style={styles.title} numberOfLines={1}>
             {title}
