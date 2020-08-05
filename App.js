6b47a54c35e2dfade687d17b6dfe8433ac77487d
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-05 23:36:57 +0200

    render login screen

--- App.js
+++ App.js
@@ -1,73 +1,11 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Screen } from 'react-native-screens';
+import LoginScreen from './app/screens/LoginScreen';
 
-import Screen from './app/components/Screen';
-import AuthNavigator from './app/navigation/AuthNavigator';
-import NavigationTheme from './app/navigation/NavigationTheme';
-import AppNavigator from './app/navigation/AppNavigator';
-import OfflineNotice from './app/components/OfflineNotice';
-
-const Link = () => {
-  const navigation = useNavigation();
-
+const App = () => {
   return (
-    <Button
-      title='Click'
-      onPress={() => navigation.navigate('TweetDetails', { id: 1 })}
-    />
+    <Screen>
+      <LoginScreen></LoginScreen>
+    </Screen>
   );
 };
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: 'dodgerblue' },
-      headerTintColor: 'white',
-    }}>
-    <Stack.Screen name='Tweets' component={Tweets} />
-    <Stack.Screen name='TweetDetails' component={TweetDetails} />
-  </Stack.Navigator>
-);
-
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name='Feed' component={FeedNavigator} />
-    <Tab.Screen name='Account' component={Account} />
-  </Tab.Navigator>
-);
-
-export default function App() {
-  return (
-    <>
-      <OfflineNotice />
-      <NavigationContainer theme={NavigationTheme}>
-        <AppNavigator />
-      </NavigationContainer>
-    </>
-  );
-}
