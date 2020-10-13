04919c3bc3739cc505cdefc7b426f63c3904665c
JasonNelemans <jasonnelemans@hotmail.com>
2020-10-13 12:18:11 +0200

    implemented login functionality

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ export default function App() {
     <>
       <OfflineNotice />
       <NavigationContainer theme={NavigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
