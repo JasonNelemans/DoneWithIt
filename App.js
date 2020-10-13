81c44dbccde3add4bece0885418bc76078980380
JasonNelemans <jasonnelemans@hotmail.com>
2020-10-13 14:40:04 +0200

    store user object in React Context and read in App

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
@@ -10,6 +10,7 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 import NavigationTheme from './app/navigation/NavigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthContext from './app/auth/context';
 
 const Link = () => {
   const navigation = useNavigation();
@@ -62,12 +63,14 @@ const TabNavigator = () => (
 );
 
 export default function App() {
+  const [user, setUser] = useState();
+
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={NavigationTheme}>
-        <AuthNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
